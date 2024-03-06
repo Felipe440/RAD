408cf53074890fde778a5f18f5188fd07158feeb
FELIPE ROCHA COELHO <202309478951@alunos.estacio.br>
2024-03-06 08:56:29 -0300

    novos

--- main.py
+++ main.py
@@ -22,8 +22,46 @@ print(arquivo.closed)'''
 # arquivo.write('banana \n uva \n mamao')
 # arquivo.close()
 
-precos = [20,100,500,600]
-with open("precos_roupas.txt","w") as arquivo:
-    for preco in precos:
-        arquivo.write(str(preco) + '\n')
-print(arquivo.closed)
+# disciplinas = [ 'RAD \n', 'introdução a C \n', 'programação']
+# with open('disciplinas.txt', 'w') as file:
+#     file.write( "relaçao de discplinas \n")
+#     file.writelines(disciplinas)
+# with open('disciplinas.txt', 'r') as file:
+#     print(file.read())
+
+# with open('precos_roupas.txt', 'r') as arquivo:
+#     print("representação original da linha")
+#     for linha in arquivo:
+#         print(repr(linha))
+
+# with open("precos_roupas.txt", "r") as arquivo:
+#     print('conteudo da linha')
+#     for linha in arquivo:
+#         linha_ = linha.strip()
+#         print(repr(linha_))
+
+'''minha_lista = ['arroz', 'feijao','carne']
+lista_ = ';'.join(minha_lista)
+with open('precos_roupas.txt','w') as arquivo:
+    arquivo.write(lista_)'''
+
+# try:
+#     with open("arquivo_test.txt", 'r') as arquivo:
+#         print(arquivo.read())
+# except FileNotFoundError:
+#     print("arquivo inexistente")
+
+import os
+
+# try:
+#     os.remove('teste.txt')
+#     print('arquviod removido')
+# except FileNotFoundError as erro:
+#     print("arquivo inexistente")
+#     print('descrição: ', erro)
+
+try:
+    f = open('novo2.txt','r')
+    f.write('hello')
+except IOError as erro:
+    print('erro foi: ', erro)
